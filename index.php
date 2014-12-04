541ba53e2cee333d16ada659941cfbd3dc53e3ba
PasiV <pasi.vanhala@metropolia.fi>
2014-12-04 20:55:13 +0200

    initial

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-phpinfo();
-?>
+date_default_timezone_set('Europe/Helsinki');
+$x = 1;
+ 
+while($x <= 100) {
+   echo "The number is: $x " . chr(9);
+
+   echo date('h:i:s') . chr(10);
+
+   //sleep for 5 seconds
+   sleep(5);
+
+   $x++;
+}
+?>  
